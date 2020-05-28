834a07b60ac71f6d459e35276b3758e193b1d00a
Arsenalisreal <59465406+Arsenalisreal@users.noreply.github.com>
2020-05-29 00:58:57 +0530

    Create Fibonacci_modified.py

---
+++ Fibonacci_modified.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fibonacciModified function below.
+def fibonacciModified(t1, t2, n):
+    if n!=0 and n-3!=0:
+        t3=t1+pow(t2,2)
+        return fibonacciModified(t2,t3,n-1)
+    if n-3==0:
+        return t1+pow(t2,2)
+
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t1T2n = input().split()
+
+    t1 = int(t1T2n[0])
+
+    t2 = int(t1T2n[1])
+
+    n = int(t1T2n[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
